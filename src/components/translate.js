0a9124885e64399e51bcc2bdef763a2dc94d47c1
selene <selene.banderas.466@gmail.com>
2021-10-30 21:13:09 +0700

    update api key

--- src/components/translate.js
+++ src/components/translate.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const doTranslation = async (input, languageCode, cancelToken) => {
   try {
     const { data } = await axios.post(
-      "https://translation.googleapis.com/language/translate/v2?key=AIzaSyCf0Xy0OnhxlduyEt3K8zP-sOuu-l_u6uA",
+      "https://translation.googleapis.com/language/translate/v2?key=AIzaSyCen4zJGTKP-pfqbP7ZCAxd1pUg7YsCAf4",
       {
         q: input,
         target: languageCode
